787a888649a7e717b10307105fdee1f01357ddf9
doarb <damienboucher25@gmail.com>
2023-11-27 10:05:03 +0100

    add model dpe

---
+++ models/DPE.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const DPESchema = new mongoose.Schema({
+    "N°_département_(BAN)": Number,
+    Date_réception_DPE: Date,
+    Date_établissement_DPE: Date,
+    Date_visite_diagnostiqueur: Date,
+    Etiquette_GES: String,
+    Etiquette_DPE: String,
+    Année_construction: Number,
+    Surface_habitable_logement: Number,
+    "Adresse_(BAN)": String,
+    "Code_postal_(BAN)": Number
+});
+
+module.exports = mongoose.model('dpe', DPESchema);
